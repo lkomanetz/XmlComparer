6ef23893687da84db57d05052f55df998d64f4d3
unknown <logan.komanetz@gmail.com>
2016-05-21 12:22:21 -0500

    Adds unit tests and gets structure building working.

---
+++ XmlComparer.Tests/XmlComparerTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XmlComparer.Tests {
+
+	[TestClass]
+	public class XmlComparerTests {
+
+		[TestMethod]
+		public void WhenXmlIsEqualComparerSucceeds() {
+			XmlDocument[] xmlDocs = CreateDocuments();
+			Assert.IsTrue(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[0]));
+		}
+
+		[TestMethod]
+		public void WhenXmlIsNotEqualComparerFails() {
+			XmlDocument[] xmlDocs = CreateDocuments();
+			Assert.IsFalse(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		private XmlDocument[] CreateDocuments() {
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[1] = new XmlDocument();
+			string xmlStringA = @"<root><child><grandchild>hello</grandchild></child></root>";
+			string xmlStringB = @"<root><child>hello</child></root>";
+
+			xmlDocs[0].LoadXml(xmlStringA);
+			xmlDocs[1].LoadXml(xmlStringB);
+
+			return xmlDocs;
+		}
+
+	}
+
+}
---
+++ XmlComparer/XmlComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlComparer {
+
+	public static class XmlComparer {
+		private static List<string> _xmlObjStructure = new List<string>();
+		private static int _elementsPerDocument = 0;
+
+		public static bool AreEqual(XmlDocument xmlObjA, XmlDocument xmlObjB) {
+			BuildXmlStructure(xmlObjA.FirstChild);
+			BuildXmlStructure(xmlObjB.FirstChild);
+
+			if ((_xmlObjStructure.Count % 2) != 0) {
+				return false;
+			}
+
+			_elementsPerDocument = (int)_xmlObjStructure.Count / 2;
+
+			return false;
+		}
+
+		private static void BuildXmlStructure(XmlNode node) {
+			if (node == null) {
+				return;
+			}
+
+			if (node.HasChildNodes) {
+				BuildXmlStructure(node.FirstChild);
+				_xmlObjStructure.Add(node.Name);
+			}
+		}
+
+		private static bool AreValuesEqual(XmlElement elementA, XmlElement elementB) {
+			return elementA.Value.Equals(elementB.Value);
+		}
+	}
+
+}

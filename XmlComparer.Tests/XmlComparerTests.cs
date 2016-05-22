44e8d9cef79394d6235271685500648e8e189390
unknown <logan.komanetz@gmail.com>
2016-05-22 15:10:59 -0500

    Adds complex structure comparison.

--- XmlComparer.Tests/XmlComparerTests.cs
+++ XmlComparer.Tests/XmlComparerTests.cs
@@ -10,46 +10,139 @@ namespace XmlComparer.Tests {
 
 		[TestMethod]
 		public void WhenXmlIsEqualComparerSucceeds() {
+			XmlComparer comparer = new XmlComparer();
 			XmlDocument[] xmlDocs = new XmlDocument[2];
 			xmlDocs[0] = new XmlDocument();
 			xmlDocs[0].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
 			xmlDocs[1] = new XmlDocument();
 			xmlDocs[1].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
 
-			Assert.IsTrue(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[0]));
+			Assert.IsTrue(comparer.AreEqual(xmlDocs[0], xmlDocs[0]));
 		}
 
 		[TestMethod]
 		public void WhenAttributesAreDifferentComparerFails() {
+			XmlComparer comparer = new XmlComparer();
 			XmlDocument[] xmlDocs = new XmlDocument[2];
 			xmlDocs[0] = new XmlDocument();
-			xmlDocs[0].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
+			xmlDocs[0].LoadXml(@"<root><child test='2'><grandchild>hello</grandchild></child></root>");
 			xmlDocs[1] = new XmlDocument();
 			xmlDocs[1].LoadXml(@"<root><child test='1'><grandchild>hello</grandchild></child></root>");
 
-			Assert.IsFalse(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+			Assert.IsFalse(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		[TestMethod]
+		public void WithComplexStructureThatIsTheSameComparerSucceeds() {
+			string xml = @"
+				<root>
+					<child test='1'>
+					</child>
+					<child test='2'>
+						<grandchild>Hello</grandchild>
+						<grandchild>Again</grandchild>
+					</child>
+				</root>";
+
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[0].LoadXml(xml);
+			xmlDocs[1] = new XmlDocument();
+			xmlDocs[1].LoadXml(xml);
+
+			Assert.IsTrue(new XmlComparer().AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		[TestMethod]
+		public void WithComplexStructureThatAreDifferentComparerFails() {
+			string xmlA = @"
+				<root>
+					<child test='1'>
+					</child>
+					<child test='2'>
+						<grandchild>Hello</grandchild>
+						<grandchild>Again</grandchild>
+					</child>
+				</root>";
+			string xmlB = @"
+				<root>
+					<child test='1'>
+					</child>
+					<child test='3'>
+						<grandchild>Hello</grandchild>
+						<grandchild>Kid</grandchild>
+					</child>
+				</root>";
+
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[0].LoadXml(xmlA);
+			xmlDocs[1] = new XmlDocument();
+			xmlDocs[1].LoadXml(xmlB);
+
+			Assert.IsFalse(new XmlComparer().AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		[TestMethod]
+		public void WhenXmlHasGroupsAndAreTheSameComparerSucceeds() {
+			XmlComparer comparer = new XmlComparer();
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[0].LoadXml(@"<root><child><grandchild>hello</grandchild></child><child></child></root>");
+			xmlDocs[1] = new XmlDocument();
+			xmlDocs[1].LoadXml(@"<root><child><grandchild>hello</grandchild></child><child></child></root>");
+
+			Assert.IsTrue(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		[TestMethod]
+		public void WhenAttributesAreTheSameComparerSucceeds() {
+			XmlComparer comparer = new XmlComparer();
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[0].LoadXml(@"<root><child test='1'><grandchild>hello</grandchild></child></root>");
+
+			xmlDocs[1] = new XmlDocument();
+			xmlDocs[1].LoadXml(@"<root><child test='1'><grandchild>hello</grandchild></child></root>");
+
+			Assert.IsTrue(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+		}
+
+		[TestMethod]
+		public void WhenValuesAreDifferentComparerFails() {
+			XmlComparer comparer = new XmlComparer();
+			XmlDocument[] xmlDocs = new XmlDocument[2];
+			xmlDocs[0] = new XmlDocument();
+			xmlDocs[0].LoadXml(@"<root><child><grandchild>test</grandchild></child></root>");
+			xmlDocs[1] = new XmlDocument();
+			xmlDocs[1].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
+
+			Assert.IsFalse(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
 		}
 
 		[TestMethod]
 		public void WhenXmlIsNotEqualComparerFails() {
+			XmlComparer comparer = new XmlComparer();
 			XmlDocument[] xmlDocs = new XmlDocument[2];
 			xmlDocs[0] = new XmlDocument();
 			xmlDocs[0].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
 			xmlDocs[1] = new XmlDocument();
 			xmlDocs[1].LoadXml(@"<root><child>hello</child></root>");
-			Assert.IsFalse(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+			Assert.IsFalse(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
 		}
 
 		[TestMethod]
 		public void WhenXmlStructureIsEqualButValuesDifferentComparerFails() {
+			XmlComparer comparer = new XmlComparer();
 			XmlDocument[] xmlDocs = new XmlDocument[2];
 			xmlDocs[0] = new XmlDocument();
 			xmlDocs[0].LoadXml(@"<root><child><grandchild>hello</grandchild></child></root>");
 			xmlDocs[1] = new XmlDocument();
 			xmlDocs[1].LoadXml(@"<root><child>hello<grandchild></grandchild></child></root>");
 
-			Assert.IsFalse(XmlComparer.AreEqual(xmlDocs[0], xmlDocs[1]));
+			Assert.IsFalse(comparer.AreEqual(xmlDocs[0], xmlDocs[1]));
 		}
+
 	}
 
 }
--- XmlComparer/XmlComparer.cs
+++ XmlComparer/XmlComparer.cs
@@ -8,142 +8,104 @@ using System.Threading.Tasks;
 
 namespace XmlComparer {
 
-	public static class XmlComparer {
-		private static Dictionary<string, string> _attributeStructure = new Dictionary<string,string>();
-		private static List<string> _valueStructure = new List<string>();
-		private static List<string> _xmlObjStructure = new List<string>();
-		private static int _elementsPerDocument = 0;
+	public class XmlComparer {
+		private Dictionary<string, string> _attributeStructure = new Dictionary<string,string>();
+		private Dictionary<Guid, bool> _results = new Dictionary<Guid, bool>();
 
-		public static bool AreEqual(XmlDocument xmlObjA, XmlDocument xmlObjB) {
-			BuildXmlStructure(xmlObjA.FirstChild);
-			BuildXmlStructure(xmlObjB.FirstChild);
+		public bool AreEqual(XmlDocument xmlObjA, XmlDocument xmlObjB) {
+			Compare(xmlObjA.FirstChild, xmlObjB.FirstChild);
+			bool isDifferent = _results.Any(kvp => kvp.Value == false);
 
-			bool areTheSame = Compare(xmlObjA.FirstChild, xmlObjB.FirstChild);
-
-			return areTheSame;
+			if (isDifferent) {
+				return false;
+			}
+			else {
+				return true;
+			}
 		}
 
-		private static bool Compare(XmlNode nodeA, XmlNode nodeB) {
+		private void Compare(XmlNode nodeA, XmlNode nodeB) {
+			bool areTheSame = true;
 			if ((nodeA == null && nodeB != null) ||
 				(nodeA != null && nodeB == null)) {
 
-				return false;
+				areTheSame = false;
 			}
 
 			if (nodeA.HasChildNodes && nodeB.HasChildNodes) {
 				Compare(nodeA.FirstChild, nodeB.FirstChild);
 			}
 
+			if (nodeA.NextSibling != null && nodeB.NextSibling != null) {
+				Compare(nodeA.NextSibling, nodeB.NextSibling);
+			}
+
 			if (!nodeA.Name.Equals(nodeB.Name)) {
+				areTheSame = false;
+			}
+
+			bool nodeAHasAttributes = HasAttributes(nodeA);
+			bool nodeBHasAttributes = HasAttributes(nodeB);
+
+			if (nodeAHasAttributes && nodeBHasAttributes) {
+				bool attributesAreTheSame = AreAttributesTheSame(nodeA.Attributes, nodeB.Attributes);
+
+				if (!attributesAreTheSame) {
+					areTheSame = false;
+				}
+			}
+
+			if ((nodeAHasAttributes && !nodeBHasAttributes) ||
+				(!nodeAHasAttributes && nodeBHasAttributes)) {
+
+				areTheSame = false;
+			}
+
+			if (nodeA.Value != null || nodeB.Value != null) {
+				areTheSame = AreValuesTheSame(nodeA.Value, nodeB.Value);
+			}
+
+			_results.Add(Guid.NewGuid(), areTheSame);
+		}
+
+		private bool HasAttributes(XmlNode node) {
+			return node.Attributes != null && node.Attributes.Count > 0;
+		}
+
+		private bool AreValuesTheSame(string objA, string objB) {
+			if ((objA == null && objB != null) ||
+				(objA != null && objB == null)) {
+
 				return false;
 			}
 
-			if (HasAttributes(nodeA) && HasAttributes(nodeB)) {
-				if (nodeA.Attributes.Count == nodeB.Attributes.Count) {
-					for (int i = 0; i < nodeA.Attributes.Count; i++) {
-						if (!nodeA.Attributes[i].Name.Equals(nodeB.Attributes[i].Name)) {
-							return false;
-						}
+			return objA == objB;
+		}
 
-						if (!nodeA.Attributes[i].Value.Equals(nodeB.Attributes[i].Value)) {
-							return false;
-						}
-					}
-				}
-				else {
+		private bool AreAttributesTheSame(XmlAttributeCollection collectionA, XmlAttributeCollection collectionB) {
+			if ((collectionA == null && collectionB != null) ||
+				(collectionA != null && collectionB == null)) {
+
+				return false;
+			}
+
+			if (collectionA.Count != collectionB.Count) {
+				return false;
+			}
+
+			for (int i = 0; i < collectionA.Count; i++) {
+				if (!collectionA[i].Name.Equals(collectionB[i].Name)) {
 					return false;
 				}
-			}
-			else {
-				return false;
+
+				if (!collectionA[i].Value.Equals(collectionB[i].Value)) {
+					return false;
+				}
 			}
 
 			return true;
 		}
 
-		private static bool HasAttributes(XmlNode node) {
-			return node.Attributes != null && node.Attributes.Count > 0;
-		}
-
-		private static bool AreAttributesTheSame(XmlAttributeCollection collectionA, XmlAttributeCollection colletionB) {
-			return false;
-		}
-
-		private static void BuildXmlStructure(XmlNode node) {
-			if (node == null) {
-				return;
-			}
-
-			if (node.HasChildNodes) {
-				BuildXmlStructure(node.FirstChild);
-				_xmlObjStructure.Add(node.Name);
-			}
-		}
-
-		private static void BuildAttributeStructure(XmlNode node) {
-			if (node == null) {
-				return;
-			}
-
-			if (node.HasChildNodes) {
-				BuildAttributeStructure(node.FirstChild);
-			}
-
-			if (node.Attributes != null &&
-				node.Attributes.Count > 0) {
-
-				for (int i = 0; i < node.Attributes.Count; i++) {
-					_attributeStructure.Add(node.Attributes[i].Name, node.Attributes[i].Value);
-				}
-			}
-		}
-
-		private static void BuildValueStructure(XmlNode node) {
-			if (node == null) {
-				 return;
-			}
-
-			if (node.HasChildNodes) {
-				BuildValueStructure(node.FirstChild);
-				_valueStructure.Add(node.InnerText);
-			}
-		}
-
-		private static void SeparateStructures() {
-		}
-
-		private static bool AreStructuresEqual() {
-			bool structuresAreEqual = true;
-			List<string> firstStructure = new List<string>();
-			List<string> secondStructure = new List<string>();
-
-			for (int i = 0; i < _xmlObjStructure.Count; i++) {
-				if (i <= _elementsPerDocument - 1) {
-					firstStructure.Add(_xmlObjStructure[i]);
-				}
-				else {
-					secondStructure.Add(_xmlObjStructure[i]);
-				}
-			}
-
-			if (firstStructure.Count != secondStructure.Count) {
-				return false;
-			}
-
-			for (int i = 0; i < _elementsPerDocument; i++) {
-				structuresAreEqual = firstStructure[i].Equals(secondStructure[i]);
-
-				if (!structuresAreEqual) {
-					break;
-				}
-			}
-
-			return structuresAreEqual;
-		}
-
-		private static bool AreValuesEqual(XmlElement elementA, XmlElement elementB) {
-			return elementA.Value.Equals(elementB.Value);
-		}
 	}
 
 }

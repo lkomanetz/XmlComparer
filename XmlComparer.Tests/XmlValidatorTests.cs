4bb3f3e580df3099f8f0eb36749b4389e4cd136e
unknown <logan.komanetz@gmail.com>
2016-05-24 19:08:12 -0500

    Starts work for a comparison class.

---
+++ XmlComparer.Tests/XmlValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlComparer.Tests {
+
+	[TestClass]
+	public class XmlValidatorTests {
+		
+		[TestMethod]
+		public void Test() {
+			string xml = @"<root><child><grandchild>hello</grandchild></child></root>";
+			XmlValidator validator = new XmlValidator(xml);
+			var node = validator.Xml
+				.Where(x => x.Name.Equals("grandchild") && x.Value.Equals("hello"))
+				.SingleOrDefault();
+
+			Assert.IsNotNull(node);
+		}
+
+	}
+
+}
---
+++ XmlComparer/XmlValidator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Threading.Tasks;
+
+namespace XmlComparer {
+
+	public class XmlValidator {
+		private XmlDocument _xmlDoc;
+		private List<XmlNode> _flattenedXml;
+
+		private XmlValidator() {
+			_xmlDoc = new XmlDocument();
+			_flattenedXml = new List<XmlNode>();
+		}
+
+		public XmlValidator(string xml) :
+			base() {
+
+			_xmlDoc.LoadXml(xml);
+			this.Flatten(_xmlDoc);
+		}
+
+		public XmlValidator(XmlDocument xml) :
+			base() {
+
+			_xmlDoc = xml;
+			this.Flatten(_xmlDoc.FirstChild);
+		}
+
+		public XmlValidator(XDocument xml) :
+			base() {
+
+			using (var xmlReader = xml.CreateReader()) {
+				_xmlDoc.Load(xmlReader);
+			}
+			this.Flatten(_xmlDoc.FirstChild);
+		}
+
+		public List<XmlNode> Xml {
+			get { return _flattenedXml; }
+		}
+
+		private void Flatten(XmlNode node) {
+			if (node == null) {
+				return;
+			}
+
+			if (node.NextSibling != null) {
+				Flatten(node.NextSibling);
+			}
+			
+			_flattenedXml.Add(node);
+		}
+	}
+
+}
